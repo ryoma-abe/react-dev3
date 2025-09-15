eff49757e80d3c2c2b1ef29486f2339dddceab6f
ryoma-abe <aryuu.appe@gmail.com>
2025-09-16 08:47:19 +0900

    ダークテーマのスタイルをindex.cssに追加し、ModeButtonコンポーネントでテーマ変更に応じてbodyクラスを更新するuseEffectフックを実装しました。

--- src/components/ModeButton.tsx
+++ src/components/ModeButton.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useTheme } from "../context/ThemeContext";
 
 export default function ModeButton() {
@@ -6,6 +7,16 @@ export default function ModeButton() {
   const toggleTheme = () => {
     setTheme(theme === "light" ? "dark" : "light");
   };
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.body.classList.add("dark");
+      document.body.classList.remove("light");
+    } else {
+      document.body.classList.add("light");
+      document.body.classList.remove("dark");
+    }
+  }, [theme]);
 
   return (
     <button className="border pl-2" onClick={toggleTheme}>
