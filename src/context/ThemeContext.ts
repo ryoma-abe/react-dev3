ff67d7161ae5ebea1049b9c8b3ae8649a09a247f
ryoma-abe <aryuu.appe@gmail.com>
2025-09-16 07:25:08 +0900

    ThemeContextに型定義を追加し、ThemeProviderコンポーネントを実装しました。

--- src/context/ThemeContext.ts
+++ src/context/ThemeContext.ts
@@ -1,7 +1,14 @@
 import { createContext, useContext } from "react";
 
+type ThemeContextType = {
+  theme: string;
+  setTheme: React.Dispatch<React.SetStateAction<string>>;
+};
+
 // 箱を作る
-export const ThemeContext = createContext(null);
+export const ThemeContext = createContext<ThemeContextType | undefined>(
+  undefined
+);
 
 // 鍵を作る
 export const useTheme = () => useContext(ThemeContext);
--- src/context/ThemeProvider.tsx
+++ src/context/ThemeProvider.tsx
@@ -1,1 +1,15 @@
 //実際の処理
+
+import { useState, type ReactNode } from "react";
+import { ThemeContext } from "./ThemeContext";
+
+
+
+export function ThemeProvider({ children }: { children: ReactNode }) {
+  const [theme, setTheme] = useState("light");
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
