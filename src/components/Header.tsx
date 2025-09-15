6f4b6f8f713eb8a12a77cbc5fdd204402b767013
ryoma-abe <aryuu.appe@gmail.com>
2025-09-16 07:42:13 +0900

    Tailwind CSSをプロジェクトに統合し、Headerコンポーネントのスタイルを更新しました。また、関連する依存関係を追加しました。

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router";
 export default function Header() {
   return (
     <header>
-      <nav>
+      <nav className="flex gap-2 p-10">
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
       </nav>
