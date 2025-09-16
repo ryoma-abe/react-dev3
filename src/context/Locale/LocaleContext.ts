e1cc5558a95afa5ae0638a6580b95ad31b4f1cb0
ryoma-abe <aryuu.appe@gmail.com>
2025-09-17 07:51:45 +0900

    LocaleContextを作成し、ロケールの状態管理を追加しました。

---
+++ src/context/Locale/LocaleContext.ts
@@ -0,0 +1,19 @@
+import { createContext, useContext } from "react";
+
+type LocaleContextType = {
+  locale: string;
+  setLocale: (value: string) => void;
+};
+
+// 箱を作る
+export const LocaleContext = createContext<LocaleContextType | undefined>(
+  undefined
+);
+
+// 鍵を作る
+
+export function useLocale() {
+  const ctx = useContext(LocaleContext);
+  if (!ctx) throw new Error("useLocale must be used within ThemeProvider");
+  return ctx;
+}
